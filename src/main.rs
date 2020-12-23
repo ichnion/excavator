deb32a7d5ba6f93665e576e575afb3c8bac90cdb
chansuke <chansuke@georepublic.de>
2020-12-23 16:30:09 +0900

    Recursively find the file from an argument

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::trackpoints::places::SavedPlace;
-use std::path::PathBuf;
 use structopt::StructOpt;
+use walkdir::WalkDir;
 
 mod trackpoints;
 
@@ -8,14 +8,27 @@ mod trackpoints;
 #[structopt(name = "excavator")]
 struct Opt {
     command: String,
-    #[structopt(parse(from_os_str))]
-    file: PathBuf,
+    file_name: String,
+    //#[structopt(parse(from_os_str))]
+    //file: PathBuf,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Opt::from_args();
-    let rawdata = std::fs::read_to_string(&args.file)?;
-    let result: SavedPlace = serde_json::from_str(&rawdata)?;
-    println!("{:?}", result);
+    let file_name = &args.file_name;
+
+    for entry in WalkDir::new(".")
+        .follow_links(true)
+        .into_iter()
+        .filter_map(|e| e.ok())
+    {
+        let f_name = entry.file_name().to_string_lossy();
+        if f_name.starts_with(file_name) {
+            let rawdata = std::fs::read_to_string(&entry.path())?;
+            let result: SavedPlace = serde_json::from_str(&rawdata)?;
+            println!("{:?}", result);
+        }
+    }
+
     Ok(())
 }

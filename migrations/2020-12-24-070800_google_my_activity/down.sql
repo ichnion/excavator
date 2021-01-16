8e049ad309d6aae9b8832cf65f14389f3d4ecb9a
Mario Basa <mario.basa@gmail.com>
2021-01-16 13:44:12 +0900

    deleted unused migration directory

--- migrations/2020-12-24-070800_google_my_activity/down.sql
+++
@@ -1,9 +0,0 @@
---
--- Tables for Google derrived data
---
-drop table google_my_activity;
-drop table activity_location_info;
-drop table activity_sub_title;
-drop table activity_details;
-drop table activity_products;
-drop table location_history;

aebf15138444bc37229be39c03988017698f16ab
chansuke <chansuke@georepublic.de>
2021-03-25 11:37:50 +0900

    Change owner name

--- .github/script/prs.js
+++ .github/script/prs.js
@@ -11,7 +11,7 @@ module.exports = async ({github, path}) => {
     console.log(logs);
 
     const { data } = await github.pulls.list({
-        owner: 'chansuke',
+        owner: 'ichnion',
         repo: 'excavator',
         base: 'develop',
         state: 'closed',

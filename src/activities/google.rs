5f3a706a8f2b1f01390c8cbba5c8ab22f674c1a3
Gawen Monnot <79226149+GawenMonnot@users.noreply.github.com>
2021-05-26 18:49:56 +0200

    Update google.rs

--- src/activities/google.rs
+++ src/activities/google.rs
@@ -1,11 +1,11 @@
+pub mod google_fit_activity;
 pub mod location_history;
 pub mod my_activity;
 pub mod saved_places;
 pub mod semantic_location_history;
-pub mod google_fit_activity;
 
+pub use google_fit_activity::*;
 pub use location_history::*;
 pub use my_activity::*;
 pub use saved_places::*;
 pub use semantic_location_history::*;
-pub use google_fit_activity::*;

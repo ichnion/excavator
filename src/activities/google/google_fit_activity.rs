59a9c829fc9d38b91c711ac870780710dca2a86e
Gawen Monnot <79226149+GawenMonnot@users.noreply.github.com>
2021-05-26 18:41:01 +0200

    Update google_fit_activity.rs

--- src/activities/google/google_fit_activity.rs
+++ src/activities/google/google_fit_activity.rs
@@ -5,35 +5,38 @@ use uuid::Uuid;
 #[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 pub struct Fit {
-    pub duration         : String,
-    pub startTime        : String,
-    pub endTime          : String,
-    pub fitnessActivity  : String
+    pub duration: String,
+    pub startTime: String,
+    pub endTime: String,
+    pub fitnessActivity: String,
 }
 
 #[allow(non_snake_case)]
 impl Fit {
     pub fn saveToDb(&self, conn: &Connection) -> Result<(), rusqlite::Error> {
-            let my_uuid = Uuid::new_v4();
-            let duration = &self.duration;
-            let startTime = &self.startTime;
-            let endTime = &self.endTime;
-            let fitnessActivity = &self.fitnessActivity;
+        //for elem in self.fit.iter() {
+        let my_uuid = Uuid::new_v4();
+        let duration = &self.duration;
+        let startTime = &self.startTime;
+        let endTime = &self.endTime;
+        let fitnessActivity = &self.fitnessActivity;
 
-            conn.execute(
-                "INSERT into google_fit_activity
+        conn.execute(
+            "INSERT into google_fit_activity
                 (uuid, activity, start, end, timestamp)
                 values($1, $2, $3, $4, $5)",
-                params![
-                    &my_uuid.to_string(),
-                    fitnessActivity,
-                    startTime,
-                    endTime,
-                    duration
-                ],
-            )
-            .map_err(|err| println!("{:?}", err))
-            .ok();
+            params![
+                &my_uuid.to_string(),
+                fitnessActivity,
+                startTime,
+                endTime,
+                duration
+            ],
+        )
+        .map_err(|err| println!("{:?}", err))
+        .ok();
+        // }
+
         Ok(())
     }
 }
@@ -46,10 +49,10 @@ mod tests {
     fn test_google_fit_activity() -> Result<(), Box<dyn std::error::Error>> {
         let conn = Connection::open("ichnion.db")?;
         let fit = Fit {
-            duration         : "".to_string(),
-            startTime        : "".to_string(),
-            endTime          : "".to_string(),
-            fitnessActivity  : "".to_string()
+            duration: "".to_string(),
+            startTime: "".to_string(),
+            endTime: "".to_string(),
+            fitnessActivity: "".to_string(),
         };
 
         let result = Fit::saveToDb(&fit, &conn);
@@ -59,3 +62,4 @@ mod tests {
         Ok(())
     }
 }
+

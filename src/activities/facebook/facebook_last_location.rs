5a3407695f61d3d62b0cf291cae01ad792dc1751
Gawen Monnot <79226149+GawenMonnot@users.noreply.github.com>
2021-05-31 11:08:40 +0200

    Update facebook_last_location.rs

--- src/activities/facebook/facebook_last_location.rs
+++ src/activities/facebook/facebook_last_location.rs
@@ -19,27 +19,53 @@ pub struct Coordinate {
 #[allow(non_snake_case)]
 #[derive(Deserialize, Debug)]
 pub struct LastLocation {
-  pub last_location_v2: LastLocationObjects
+  pub last_location_v2: Option<LastLocationObjects>,
+  pub last_location: Option<LastLocationObjects>
 }
 
 #[allow(non_snake_case)]
 impl LastLocation {
     pub fn saveToDb(&self, conn: &Connection) -> Result<(), rusqlite::Error> {
-        conn.execute(
-            "INSERT INTO facebook_last_location (
-            time,
-            latitude,
-            longitude
-        )
-        VALUES (?1, ?2, ?3)",
-            params![
-                &self.last_location_v2.time.to_string(),
-                &self.last_location_v2.coordinate.latitude.to_string(),
-                &self.last_location_v2.coordinate.longitude.to_string()
-            ],
-        )
-        .map_err(|err| println!("{:?}", err))
-        .ok();
+        match &self.last_location_v2 {
+            None => None,
+            Some(x) => Some(
+                conn.execute(
+                    "INSERT INTO facebook_last_location (
+                        time,
+                        latitude,
+                        longitude
+                    )
+                    VALUES (?1, ?2, ?3)",
+                    params![
+                        &x.time.to_string(),
+                        &x.coordinate.latitude.to_string(),
+                        &x.coordinate.longitude.to_string()
+                    ],
+                )
+                .map_err(|err| println!("{:?}", err))
+                .ok(),
+            ),
+        };
+        match &self.last_location {
+            None => None,
+            Some(x) => Some(
+                conn.execute(
+                    "INSERT INTO facebook_last_location (
+                        time,
+                        latitude,
+                        longitude
+                    )
+                    VALUES (?1, ?2, ?3)",
+                    params![
+                        &x.time.to_string(),
+                        &x.coordinate.latitude.to_string(),
+                        &x.coordinate.longitude.to_string()
+                    ],
+                )
+                .map_err(|err| println!("{:?}", err))
+                .ok(),
+            ),
+        };
 
         Ok(())
     }
@@ -49,6 +75,26 @@ impl LastLocation {
 mod tests {
     use super::*;
     use rusqlite::Connection;
+
+    #[test]
+    fn test_last_l_v2() -> Result<(), Box<dyn std::error::Error>> {
+        let conn = Connection::open("ichnion.db")?;
+        let test_coordinate = Coordinate {
+            latitude: 10.0,
+            longitude: 10.0,
+        };
+        let test_last_location1 = LastLocationObjects {
+            time: 1,
+            coordinate: test_coordinate,
+        };
+        let test_last_location = LastLocation {
+            last_location_v2: Some(test_last_location1),
+            last_location: None,
+        };
+        let result = LastLocation::saveToDb(&test_last_location, &conn);
+        assert!(result == Ok(()));
+        Ok(())
+    }
 
     #[test]
     fn test_last_l() -> Result<(), Box<dyn std::error::Error>> {
@@ -62,7 +108,8 @@ mod tests {
             coordinate: test_coordinate,
         };
         let test_last_location = LastLocation {
-            last_location_v2: test_last_location1,
+            last_location_v2: None,
+            last_location: Some(test_last_location1),
         };
         let result = LastLocation::saveToDb(&test_last_location, &conn);
         assert!(result == Ok(()));
